9fc4699e016cc77e511049cf8c6be60a204693b0
vinicodex <mvnxr28@gmail.com>
2023-05-15 14:03:54 -0300

    add tags/templatetags to controll most commented and latests posts

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManger(models.Manager):
@@ -29,6 +30,7 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManger()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ["-publish"]
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,28 @@
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from .forms import EmailPostForm, CommentForm
-from .models import Post, Comment
+from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 
-class PostListView(ListView):
-    """
-    Alternative post list view
-    """
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+def post_list(request, tag_slug=None):
+    post_list_query = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list_query = post_list_query.filter(tags__in=[tag])
+    paginator = Paginator(post_list_query, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blog/post/list.html',{'posts': posts, 'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -23,11 +31,16 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
-    comments = post.comments.filter(active=True)  # Form for users to comment
+    comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post, 'comments':comments, 'form':form})
+                  {'post': post, 'comments':comments, 'form':form, "similar_posts": similar_posts})
 
 
 def post_share(request, post_id):
