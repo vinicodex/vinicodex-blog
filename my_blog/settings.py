070e103c54aa9bbfb0237911d5063c53566fcb61
vinicodex <mvnxr28@gmail.com>
2023-05-15 18:43:14 -0300

    update allowed hosts

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -86,6 +86,8 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = ["vinicodex.pythonanywhere.com"]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
