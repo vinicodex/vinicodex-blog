24a202602b2b69565ab185fb5ec138d5814152fc
vinicodex <mvnxr28@gmail.com>
2023-06-25 21:29:52 -0300

    remove menu to projects and contacts

--- website/views.py
+++ website/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'website/index.html')
 
+
 def projects(request):
     return render(request, 'website/projects.html')
-
